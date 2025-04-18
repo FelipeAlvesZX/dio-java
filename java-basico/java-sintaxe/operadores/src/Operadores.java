4e9fd3b589f7e1416900a901340b9e2ae51cb2ca
Felipe <104693099+FelipeAlvesZX@users.noreply.github.com>
2025-04-18 16:27:36 -0300

    up operadores

---
+++ java-basico/java-sintaxe/operadores/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        // Tipos primitivos
+        // byte, short, int, long, float, double, char, boolean
+
+        String nome = "Felipe";
+        int idade = 20;
+        double peso = 100.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        // Date dataNascimento = new Date(); // new Date() retorna a data atual
+
+
+        // operadores aritméticos
+        // +, -, *, /, % (resto da divisão)
+        double soma = 10.5 + 15.7;
+        int subtracao = 113- 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3; // resto da divisão
+        double resultado = (10 * 7) + (20 / 4);
+
+
+        String nomeCompleto = "Linguagem" + " Java";
+
+        System.out.println(nomeCompleto);
+
+        
+         
+    }
+}
