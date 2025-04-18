eb3e02a7c2e73c1a1ec5dbc1094a88c4e2b9d222
Felipe <104693099+FelipeAlvesZX@users.noreply.github.com>
2025-04-18 18:35:58 -0300

    up java terminal

---
+++ java-basico/java-sintaxe/Java-Terminal/src/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    /**
+     * Este método é utilizado para soma dois números inteiros.
+     * @param numeroUm este é o primeiro parametro do metodo
+     * @param numeroDois este é o segundo parametro do metodo
+     * @return int o resultado deste método é a soma dos dois números. 
+     */ 
+
+    public int soma(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ java-basico/java-sintaxe/Java-Terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios{
+    public static void main(String[] args) {
+        // óla, eu sou um comentário de uma única linha
+
+        /*
+        Eu sou um comentário
+        que possa ser mais detalhado
+        e ocupar mais de uma linha.
+        */
+
+    }
+    /**
+    * Estas duas estrelinhas acima
+    * é para indetificar que você
+    * pretende elaborar um comentário
+    * a nível de documentação.	
+    */
+    public void metodo(){
+
+    }
+}
