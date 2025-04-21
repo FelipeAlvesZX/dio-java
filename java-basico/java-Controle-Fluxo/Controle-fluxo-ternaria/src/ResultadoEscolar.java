da1ded34b6cc079d7137d08d2655a127b56a0310
Felipe <104693099+FelipeAlvesZX@users.noreply.github.com>
2025-04-21 20:32:56 -0300

    up fluxo ternario

---
+++ java-basico/java-Controle-Fluxo/Controle-fluxo-ternaria/src/ResultadoEscolar.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) throws Exception {
+        int nota = 7; // Exemplo de nota
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
